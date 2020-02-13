554fea9442f8d866937983190ddf69150ebc3dd9
pappadappa <40058703+pappadappa@users.noreply.github.com>
2020-02-13 16:02:55 +0700

    first time for coding deep learning in our senior project

---
+++ ML_tensor.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 13 15:54:51 2020
+
+@author: Chonthicha Pinkaraket
+"""
+
+# import numpy as np
+# np.random.seed(123)  # for reproducibility
+ 
+# from keras.models import Sequential
+# from keras.layers import Dense, Dropout, Activation, Flatten
+# from keras.layers import Convolution2D, MaxPooling2D
+# from keras.utils import np_utils
+# from keras.datasets import mnist
+
+import tensorflow as tf
+import numpy as np
+print(tf.__version__)
